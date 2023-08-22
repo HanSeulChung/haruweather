79819cb9bcac5baecf646e38ac1a93c722344b88
HanSeul <w0w1278@naver.com>
2023-08-23 08:10:50 +0900

    swagger 사용을 위해 spring boot version 3.0.4 -> 2.7.4로 다운그레이드

--- src/main/java/zerobase/weather/WeatherApplication.java
+++ src/main/java/zerobase/weather/WeatherApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableTransactionManagement
 @EnableScheduling
+@EnableSwagger2
+@EnableOpenApi
 public class WeatherApplication {
 
 	public static void main(String[] args) {
